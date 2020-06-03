5e9901d150a85d17e76e0505a63ce13478a19a60
Bella Buchanan <isabellabuchanan1@gmail.com>
2020-06-03 08:24:00 +0200

    added toggle feature to read button

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const add = document.getElementById('new-book');
 const formContainer = document.querySelector('.form-container');
 
 // book constructor
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    // this.read = read;
+    this.read = read ? 'Yes' : 'No';;
+
     }
 
 // prototypical inheritance to display book info
@@ -31,29 +32,61 @@ remove.addEventListener('click', function(){
 // submit form 
 const submit = document.getElementById('submit-btn');
 
-submit.addEventListener('click', function(){
-    const title = document.getElementById('book-title').value;
-    const author = document.getElementById('book-author').value;
-    const pages = document.getElementById('pages').value;
+const title = document.getElementById('book-title');
+const author = document.getElementById('book-author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('read');
+submit.addEventListener('click', function(e){
+    e.preventDefault()
+    const titleTable = title.value;
+    const authorTable = author.value;
+    const pagesTable = pages.value;
+    const readTable = read.value;
     
     // add book to library 
-    let book = new Book(title,author,pages);
+    let book = new Book(titleTable,authorTable,pagesTable, readTable);
+    
     myLibrary.push(book);
     
     // display entire library 
     displayInLibrary(myLibrary);
+    formContainer.style.display = "none"; 
+
+    
 })
 
+
 // display all books
-const tableBody = document.getElementById('table');
+const table = document.querySelector('.table');
+const tableBody = document.getElementById('table-body');
 function displayInLibrary(library) {
-    books = "";
-    library.forEach(item => {
-        const row = document.createElement('tr');
-        row.dataset.position = library.indexOf(item);
-        row.innerHTML = `
-          <td>${item.title}</td>
-          <td>${item.author}</td>
-          <td>${item.pages}</td>`
-        tableBody.appendChild(row);
-})};
+    bookAdded = library[library.length - 1];
+    console.log(bookAdded);
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${bookAdded.title}</td>
+        <td>${bookAdded.author}</td>
+        <td>${bookAdded.pages}</td>
+        <td><button class="change-status">${bookAdded.read}</button></td>`
+    tableBody.appendChild(row);
+    
+        
+};
+
+document.querySelector('.tbody').addEventListener('click', (e) => {
+    if (e.target.className === 'change-status') {
+        if (e.target.textContent === 'Yes') {
+            e.target.textContent = 'No';       
+        } else {
+            e.target.textContent = 'Yes';
+            
+        }
+    }
+})
+
+
+let aBook = new Book("test","bella","10");
+myLibrary.push(aBook);
+displayInLibrary(myLibrary);
+console.log(aBook.pages);
+
